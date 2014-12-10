b80876b62e751605545b0c942143ac05b36a051b
makakoa <makakoa15@gmail.com>
2014-12-10 11:39:22 -0800

    basic hey you demo

--- app/js/controllers/mapCtrl.js
+++ app/js/controllers/mapCtrl.js
@@ -2,12 +2,20 @@
 
 module.exports = function(app) {
   app.controller('mapCtrl', ['$scope', '$http', 'uiGmapIsReady', function($scope, $http, MapReady) {
+    $scope.dot = {
+      title: 'hey you!',
+      text: 'Click on a pin!',
+      user: 'hey you dev team',
+      show: true
+    };
+
     $scope.index = function(maps) {
       $http({
         method: 'GET',
         url: 'http://hey-you-api.herokuapp.com/v1/api/dots/all'
       })
       .success(function(data) {
+        if (data.length === 0) $scope.dot.text = 'Sorry, there are no posts right now.';
         data.forEach(function(dot) {
           var position = new maps.LatLng(dot.latitude, dot.longitude);
           var marker = new maps.Marker({
@@ -23,7 +31,9 @@ module.exports = function(app) {
             maps[0].panTo(marker.getPosition());
             $scope.dot.title = this.title;
             $scope.dot.text = this.text;
+            $scope.dot.user = this.user;
             $scope.dot.color = this.color;
+            $scope.dot.show = true;
           });
         });
       })
@@ -50,5 +60,11 @@ module.exports = function(app) {
     MapReady.promise().then(function(maps) {
       $scope.index(maps);
     });
+
+    $scope.advertise = function() {
+      $scope.dot.title = 'hey you!';
+      $scope.dot.text = 'Download hey you soon on the app store!';
+      $scope.dot.user = 'hey you dev team';
+    };
   }]);
 };
